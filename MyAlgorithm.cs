7d4a1cc3ec6fb0a6b2b00d8732853d1bfee6f72b
Alexander Belov <ipritoflex@yandex.ru>
2014-12-15 18:10:31 +0300

    Update MyAlgorithm.cs

--- MyAlgorithm.cs
+++ MyAlgorithm.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SearchWay
 {
-    class MyAlgorithm
+    class MyAlgorithm : IAlgorithm
     {
         private Graph graph;
         private const int INF = unchecked((int)10e9);
